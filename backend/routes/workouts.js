98bbcbf27752fef40297e9c3100d07c1793e1c20
abdullah <abdullah.softwaredev@gmail.com>
2025-01-05 14:00:33 +0530

    some changes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-   
+   res.json({message: "hello world"})
 })  
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,16 @@ app.use((req,res,next) => {
         console.log(req.path, req.method);
         next()
 })
-app.use(workoutsRoutes)
+// app.get('/', (req, res) => {
+//     console.log("this works")
+//     res.send('hello world');
+// })
+// app.post('/', (req, res) => {
+//     req.readOnly = true;
+// })
+app.use('/api/workouts',workoutsRoutes)
 app.listen(process.env.PORT, () => {
     console.log('listening on port', process.env.PORT);
 });
-app._router.stack.forEach((route, index) => {
-    console.log(route.route.path);
-});
+
+
